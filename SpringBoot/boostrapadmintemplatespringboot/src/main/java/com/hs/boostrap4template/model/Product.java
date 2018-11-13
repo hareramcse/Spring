e630ab73cfe801d6a714c35e456b9e7f222b073e
Hareram Singh <harsingh18@publicisgroupe.net>
2018-11-13 12:55:38 +0530

    adding some more application

---
+++ SpringBoot/boostrapadmintemplatespringboot/src/main/java/com/hs/boostrap4template/model/Product.java
@@ -0,0 +1,23 @@
+package com.hs.boostrap4template.model;
+
+public class Product {
+
+	private String fullname;
+	private String productsku;
+
+	public String getFullname() {
+		return fullname;
+	}
+
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
+	public String getProductsku() {
+		return productsku;
+	}
+
+	public void setProductsku(String productsku) {
+		this.productsku = productsku;
+	}
+}
